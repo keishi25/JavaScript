de921a74426c814eece216b64b8947de44f089c4
Your Name <you@example.com>
2020-04-27 14:23:02 +0900

    Add function of exchange variable between html and javascript in google-charts

--- JavaScript/google-charts/scatter-chart.js
+++ JavaScript/google-charts/scatter-chart.js
@@ -2,20 +2,21 @@ google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);  
 
 function drawChart() {
+	
 	var data = google.visualization.arrayToDataTable([
           ['Age', 'Weight'],
-          [ 8,      12],
-          [ 4,      5.5],
+          [ a,      b],
+          [ 40,      50.5],
           [ 11,     14],
-          [ 4,      5],
-          [ 3,      3.5],
-          [ 6.5,    7]
+          [ 40,      50],
+          [ 30,      30.5],
+          [ 6.50,    70]
         ]);
 
         var options = {
           title: 'Age vs. Weight comparison',
-          hAxis: {title: 'Age', minValue: 0, maxValue: 15},
-          vAxis: {title: 'Weight', minValue: 0, maxValue: 15},
+          hAxis: {title: 'Age1', minValue: 0, maxValue: 15},
+          vAxis: {title: 'Weight1', minValue: 0, maxValue: 15},
           legend: 'none'
         };
 
